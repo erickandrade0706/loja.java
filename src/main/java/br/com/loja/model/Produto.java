716960dd34c253047a4669579f1ce9a2dc615427
ERICK DE ANDRADE AMADO <erick.amado@aluno.senac.rio>
2026-09-21 21:26:29 -0300

    Segundo commit do projeto

--- src/main/java/br/com/loja/model/Produto.java
+++ src/main/java/br/com/loja/model/Produto.java
@@ -1,19 +1,39 @@
 package br.com.loja.model;
 
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 
+@Entity
+@Table(name="Produtos")
+
 public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Column(nullable = false, length = 255)
+	private String descricao;
+
+	@Column(nullable = false)
+	private Integer quantidade;
+
+	@Column(nullable = false, length = 100)
 	private String nome;
-	private BigDecimal preco; // Atualizado para BigDecimal
+
+	@Column(nullable = false, precision = 10, scale = 2)
+	private BigDecimal preco;// Atualizado para BigDecimal
 
 	// Construtores
 	public Produto() {}
 
-	public Produto(Long id, String nome, BigDecimal preco) {
+	public Produto(Long id, String nome, BigDecimal preco, Integer quantidade,String descricao) {
 		this.id = id;
 		this.nome = nome;
 		this.preco = preco;
+		this.quantidade = quantidade;
+		this.descricao = descricao;
 	}
 
 	// Getters e Setters
@@ -25,4 +45,10 @@ public class Produto {
 
 	public BigDecimal getPreco() { return preco; }
 	public void setPreco(BigDecimal preco) { this.preco = preco; }
+
+	public Integer getQuantidade() {return quantidade;}
+	public void setQuantidade(Integer quantidade) {this.quantidade = quantidade;}
+
+	public String getDescricao() {return descricao;}
+	public void setDescricao(String descricao) {this.descricao = descricao;}
 }
